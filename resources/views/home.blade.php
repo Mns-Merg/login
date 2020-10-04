b09d876cf841a8d7491e9f0da2fff712062ae964
Manolis <mmantakos@lartigiano.gr>
2020-10-05 00:48:18 +0300

    views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center">Welcome {{$user->name}} </div>
 
-                <div class="card-body">
+                <div class="card-body text-left">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }} {{$user->name}}
+                    In order to use the application first you need to join a class and then navigate to get quiz.<br>
 
-                    <a href="/class_join">Σύνδεση σε αίθουσα αίθουσας</a>{{ session()->get('class') }}
-                    <a href="/vote">Get Quiz for Vote
+                    <a href="/class_join">Join a Class</a><br>
+                    <a href="/vote">Get Quiz</a><br>
+                    {{ session()->get('class') }}
 
 
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>E-Quiz</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    E-Quiz
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,11 +51,17 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+
+
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
+
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/class_join">Join class</a>
+                                    <a class="dropdown-item" href="/vote">Get Quiz</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
