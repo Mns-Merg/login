703ef3f6b494ae21331423eea0458232cbfdd8da
Manolis <mmantakos@lartigiano.gr>
2020-10-07 19:46:57 +0300

    required form

--- resources/views/quizs/create.blade.php
+++ resources/views/quizs/create.blade.php
@@ -22,27 +22,27 @@
           @csrf
           <div class="form-group">    
               <label for="question">Question:</label>
-              <input type="text" class="form-control" name="question"/>
+              <input type="text" class="form-control" name="question" required/>
           </div>
           <div class="form-group">
               <label for="answer_A">Answer A:</label>
-              <input type="text" class="form-control" name="answer_A"/>
+              <input type="text" class="form-control" name="answer_A" required/>
           </div>
           <div class="form-group">
               <label for="answer_A">Answer B:</label>
-              <input type="text" class="form-control" name="answer_B"/>
+              <input type="text" class="form-control" name="answer_B" required/>
           </div>
           <div class="form-group">
               <label for="answer_A">Answer C:</label>
-              <input type="text" class="form-control" name="answer_C"/>
+              <input type="text" class="form-control" name="answer_C" required/>
           </div>
           <div class="form-group">
               <label for="answer_A">Answer D:</label>
-              <input type="text" class="form-control" name="answer_D"/>
+              <input type="text" class="form-control" name="answer_D" required/>
           </div>  
           <div class="form-group">
               <label for="answer_A">Correct Answer:</label>
-              <input type="text" class="form-control" name="correct_answer"/>
+              <input type="text" class="form-control" name="correct_answer" required/>
           </div>                
           <button type="submit" class="btn btn-primary">Add quiz</button>
       </form>
